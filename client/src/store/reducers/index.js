98b2806f5b6cec989be87866965447629d7bcfc0
amirali farzandi <33330269+amiralif@users.noreply.github.com>
2022-08-29 17:28:13 +0430

    add movie reducer to reducer combine

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { registerReducer } from "./registerReducer";
 import { loginReducer } from "./loginReducer";
+import {movieReducer} from './movieReducer'
 
 export default combineReducers({
   register: registerReducer,
   login: loginReducer,
+  movies:movieReducer,
 });
