95d419f2f877c1e6de4659caf9f9a9250cae9c26
amirali farzandi <33330269+amiralif@users.noreply.github.com>
2022-08-29 23:27:30 +0430

    change server expire token to 1H

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(jsonServer.defaults());
 
 const SECRET_KEY = "123456789";
 
-const expiresIn = "1m";
+const expiresIn = "1h";
 
 // Create a token from a payload
 function createToken(payload) {
